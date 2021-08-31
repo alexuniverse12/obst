ecee9fcf23ce2e6eb67113f93ce7045e9fcab8f3
ilya.zavidny@gmail.com <ilya.zavidny@gmail.com>
2021-08-31 18:53:54 +0300

    as

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,31 @@
 import  React from 'react';
 
 const Header = () => {
+    const isMobile = window.screen.width <= 480;
     return (
         <div className = "Header"> 
             <div className = "Logo">
                 <h1>OBST</h1>
             </div>
-            <div className = "Menu"> 
-                <div className = "MenuTag">
-                    <h2>About</h2>
-                </div>
-                <div className = "MenuTag">
-                    <h2>Smoothies</h2>
-                </div>
-                <div className = "MenuTag">
-                    <h2>Delivery</h2>
-                </div>
-                <div className = "MenuTag">
-                    <h2>Order</h2>
-                </div>
-            </div>
+            {
+                    isMobile ? <div className="burg"></div> :
+                    <div className = "Menu"> 
+                        <div className = "MenuTag">
+                            <h2>About</h2>
+                        </div>
+                        <div className = "MenuTag">
+                            <h2>Smoothies</h2>
+                        </div>
+                        <div className = "MenuTag">
+                            <h2>Delivery</h2>
+                        </div>
+                        <div className = "MenuTag">
+                            <h2>Order</h2>
+                        </div>
+                    </div>
+            }
         </div>
-    )
+    );
 }
 
 export default Header;
