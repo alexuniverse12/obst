45b9d339e1e2a42423d17253586b8c1edb85d443
Ilya Zavidny <ilya.zavidny@gmail.com>
2021-08-31 13:43:27 +0300

    producst done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Offer from "./Offer";
+import Products from "./Products";
 
 function Main() {
   return (
     <>
       <Header/>
       <Offer/>
+      <Products/>
     </>
   );
 }
