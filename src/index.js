b1c9de3ae22f6e5561808d5622e82fd78406a573
Ilya Zavidny <ilya.zavidny@gmail.com>
2021-08-31 11:07:07 +0300

    ready to work

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from "./components/Main";
 import reportWebVitals from './reportWebVitals';
+import "./styles/styles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
