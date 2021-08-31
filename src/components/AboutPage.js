0e2aa44edd0539e087dbed05ed4b05d06ccbebfd
Troy-Harder <66723101+Troy-Harder@users.noreply.github.com>
2021-08-31 12:45:23 +0200

    sv1.5

---
+++ src/components/AboutPage.js
@@ -0,0 +1,16 @@
+import  React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div className = "AboutPage"> 
+            <div id = "headline">
+                <h1>We carefuly delivery cheap and tasty smoothies in 15 mins</h1> 
+            </div>
+            <div id = "circles" >
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutPage;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Offer from "./Offer";
+import AboutPage from './AboutPage';
 
 function Main() {
   return (
     <>
       <Header/>
       <Offer/>
+      <AboutPage/>
     </>
   );
 }
