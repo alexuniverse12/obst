1f4b9db839ab0c2de3199d6876c6d0a5dbbefeef
Troy-Harder <66723101+Troy-Harder@users.noreply.github.com>
2021-08-31 14:29:20 +0200

    egr

---
+++ src/components/Delivery.js
@@ -0,0 +1,13 @@
+import  React from 'react';
+
+const Delivery = () => {
+    return (
+        <div className = "Delivery"> 
+            <div id = "headline">
+                <h1>Delivery Map</h1> 
+            </div>
+        </div>
+    )
+}
+
+export default Delivery;
