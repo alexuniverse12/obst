490e8c773a57c00ab63fe0ea97699a36c828bf64
Troy-Harder <66723101+Troy-Harder@users.noreply.github.com>
2021-08-31 13:41:08 +0200

    about finished

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -7,7 +7,21 @@ const AboutPage = () => {
                 <h1>We carefuly delivery cheap and tasty smoothies in 15 mins</h1> 
             </div>
             <div id = "circles" >
-                <div></div>
+                <div className = "oneCircle">
+                    <h1>CHEAP</h1> 
+                </div>
+                <div className = "oneCircle" id = "advancedCircle">
+                    <h1>{"FAST\nDELIVERY"}</h1> 
+                </div>
+                <div className = "oneCircle">
+                    <h1>TASTY</h1> 
+                </div>
+                <div className = "oneCircle" id = "advancedCircle">
+                <h1>{"FREE\nDELIVERY"}</h1> 
+                </div>
+                <div className = "oneCircle">
+                    <h1>NUTRICIOUS</h1> 
+                </div>
             </div>
         </div>
     )
