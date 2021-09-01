2bb4d0bffe944e37d3d04a616a2731838caea601
ilya.zavidny@gmail.com <ilya.zavidny@gmail.com>
2021-09-01 10:44:15 +0300

    anchor links

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,27 +11,43 @@ const Header = ({menu, setMenu}) => {
             {
                     isMobile ? <div className="burg" id={menu ? "active" : "disabled"} onClick={() => setMenu(!menu)}></div> :
                     <div className = "Menu"> 
-                        <div className = "MenuTag"  onClick={ () => {
-                            setMenu(false);
+                        <div className = "MenuTag" onClick={ () => {
                             const element = document.getElementsByClassName("AboutPage")[0];
-                            const y = element.getBoundingClientRect().y - 1400;
+                            const y = element.getBoundingClientRect().y + 100;
 
                             window.scrollTo({top: y, behavior: 'smooth'});
                         }
-                        }
-                        >
+                        }>
                             <h2>O Nás</h2>
                         </div>
-                        <div className = "MenuTag">
-                            <h2 >Smoothies</h2>
+                        <div className = "MenuTag" onClick={ () => {
+                            const element = document.getElementsByClassName("products")[0];
+                            const y = element.getBoundingClientRect().y - 150;
+
+                            window.scrollTo({top: y, behavior: 'smooth'});
+                        }
+                        }>
+                            <h2>Smoothies</h2>
                         </div>
-                        <div className = "MenuTag">
+                        <div className = "MenuTag" onClick={ () => {
+                            const element = document.getElementsByClassName("Delivery")[0];
+                            const y = element.getBoundingClientRect().y + 250;
+
+                            window.scrollTo({top: y, behavior: 'smooth'});
+                        }
+                        }>
                             <h2>Doručení</h2>
                         </div>
-                        <div className = "MenuTag">
+                        <div className = "MenuTag" onClick={ () => {
+                            const element = document.getElementsByClassName("order")[3];
+                            const y = element.getBoundingClientRect().y ;
+
+                            window.scrollTo({top: y, behavior: 'smooth'});
+                        }
+                        }>
                             <h2>Objednat</h2>
                         </div>
-                    </div>
+                </div>
             }
         </div>
         {
